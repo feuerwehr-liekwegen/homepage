3bd2906a57c1264989bf26c360e55dfd34cc5548
Tom Jason Schwanke <tom@catboys.cloud>
2025-04-07 20:30:22 +0200

    Artikel: LF10 Countdown; Shortcode: Countdown

---
+++ assets/js/countdown.js
@@ -0,0 +1,33 @@
+(function () {
+	endTimestamp = document.getElementsByName('end')[0].content;
+	endText = document.getElementsByName('endText')[0].content;
+	const second = 1000,
+		  minute = second * 60,
+		  hour = minute * 60,
+		  day = hour * 24;
+
+	var futureDate = new Date(endTimestamp*1000);
+		countDown = new Date(futureDate).getTime(),
+		x = setInterval(function() {    
+  
+		  let now = new Date().getTime(),
+			  distance = countDown - now;
+  
+		  document.getElementById("days").innerText = Math.floor(distance / (day)),
+			document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+			document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+			document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  
+		  //do something later when date is reached
+		  if (distance < 0) {
+			let headline = document.getElementById("headline"),
+			countdown = document.getElementById("countdown");
+  			headline.innerText = endText;
+			countdown.style.visibility = 'hidden';
+			clearInterval(x);
+		  }
+		  //seconds
+		}, 0)
+	}());
+
+	
---
+++ content/aktuelles/1744045409604-Countdown-LF10/index.md
@@ -0,0 +1,9 @@
+---
+title: "Da kommt was auf uns zu"
+date: 2025-04-10
+draft: false
+summary: "Was das wohl wird?"
+showSummary: true
+tags: ["aktuelles"]
+---
+{{< countdown end=1744308000 headline="Noch" endText="Das LF10 ist da!" >}}
